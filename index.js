faa3225935dc0bd06fba8894c00d6821d6d51220
salmanalfarizi3 <salmanalfariziiel@gmail.com>
2026-07-27 15:47:04 +0700

    feat: atur route api dan jalankan server

--- index.js
+++ index.js
@@ -5,3 +5,15 @@ const app = express();
 const PORT = 3000;
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use('/api', require('./routes/api'));
+
+async function startServer() {
+    await connectToDatabase();
+    app.listen(PORT, () => {
+        console.log(`Server is running on http://localhost:${PORT}`);
+    });
+}
+
+startServer();
